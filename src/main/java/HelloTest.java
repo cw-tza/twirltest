ad9d62144a5e7c97b8d8f96e819d92a6285aa399
Tomasz Zabłocki <tomasz.zablocki@codeweaver.pl>
2016-06-28 22:57:56 +0200

    Initial commit

---
+++ src/main/java/HelloTest.java
@@ -0,0 +1,11 @@
+import html.Hello;
+import xml.foo;
+
+class HelloTest
+{
+  public static void main(String[] args)
+  {
+    System.out.println(Hello.apply("XXX").toString());
+    System.out.println(foo.apply("XXX").toString());
+  }
+}
---
+++ src/main/java/x/TwirlConstants.java
@@ -0,0 +1,11 @@
+package x;
+
+public final class TwirlConstants
+{
+  public static final String CONST_STR=  "foo";
+  public static final int CONST_NUM =  3;
+
+  private TwirlConstants() {
+
+  }
+}
